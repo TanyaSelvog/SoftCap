b9ad130d29b969727ab47e3f552a81ef6917bb16
Tanya Selvog <tanyaselvog@gmail.com>
2022-12-31 13:44:55 -0700

    edited column

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/LoginWindow.fxml"));
-        primaryStage.setTitle("Welcome to Scheduler!");
+        primaryStage.setTitle("Welcome to SoftCap Scheduler!");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
 
